563c74939ba501f7c7b69514327a4937a96a2afb
root <root@oc1783377272.ibm.com>
2020-05-18 09:46:48 -0300

    commit

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("backend/images")));
 
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
